fdd00ca2017e5a3e8563a5e1efe5efaf2cd8fa53
Dennis Boon <dennis.boon96@gmail.com>
2019-11-27 19:48:42 +0100

    Setup basic finite state machine for turn system

---
+++ Global Health/Assets/Scripts/TurnSystem/CurrentTurnState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentTurnState : IState
+{
+    TurnManager owner;
+
+    public CurrentTurnState(TurnManager owner) { this.owner = owner; }
+
+    public void Enter()
+    {
+        Debug.Log("ENTERING CURRENT TURN STATE");
+    }
+
+    public void Execute()
+    {
+        Debug.Log("UPDATING CURRENT TURN STATE");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("EXITING CURRENT TURN STATE");
+    }
+}
---
+++ Global Health/Assets/Scripts/TurnSystem/TurnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    StateMachine stateMachine = new StateMachine();
+
+    private void Start()
+    {
+        stateMachine.ChangeState(new CurrentTurnState(this));               
+    }
+
+    private void Update()
+    {
+        stateMachine.Update();
+    }
+}
