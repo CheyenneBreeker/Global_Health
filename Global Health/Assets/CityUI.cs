513076167ee5da3eb049aec7f02c7cb81840314b
Desktop-Marcus <marcus.bicker@gmail.com>
2019-11-28 15:27:05 +0100

    Values added for cities and can be updated for UI

--- Global Health/Assets/CityUI.cs
+++ Global Health/Assets/CityUI.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CityUI : MonoBehaviour
 {
     public GameObject cityDetailsUI;
-    public void UpdateUI()
+    public Text population;
+    public Text healthCare;
+    public Text deathrate;
+    public Text economy;
+    public Text casesPrevented;
+    public Text cureSucces;
+    public void UpdateUI(int n_population, int n_healthcare, int n_deathrate, string n_economy, int n_casesPrevented, int n_cureSucces)
     {
-        //TODO: Update ui information -> population, deathrate
-    }
+        population.text = "Population: " + n_population.ToString();
+        healthCare.text = "Healthcare: " + n_healthcare.ToString();
+        deathrate.text = "Deathrate: " + n_deathrate.ToString();
+        economy.text = "Economy: " + n_economy;
+        casesPrevented.text = "Cases prevented: " + n_casesPrevented.ToString();
+        cureSucces.text = "Cure succes: " + n_cureSucces.ToString();
+
+}
 
     //Turning on the UI in gameworld
     public bool isUiActive;
--- Global Health/Assets/Scripts/City.cs
+++ Global Health/Assets/Scripts/City.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class City : MonoBehaviour
 {
+    public CityUI cityui;
     public int currentPopulation;
+    public int healthCare;
+    public int deathrate;
+    public string economy;
+    public int casesPrevented;
+    public int cureSucces;
+
+    private void Start()
+    {
+        UpdateCityUI();
+    }
 
     private void SendPopulation()
     {
         GameWorld.Instance.NewPopulation(currentPopulation);
+    }
+    
+    public void UpdateCityUI()
+    {
+        cityui.UpdateUI(currentPopulation,healthCare,deathrate,economy,casesPrevented,cureSucces);
     }
 }
