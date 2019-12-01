5e54963081d37714908e21f8e9d4290acf4a16d3
Daeneth <Daenethv@gmail.com>
2019-12-01 21:16:09 +0100

    Update

--- Global Health/Assets/Scripts/EventCardDeck.cs
+++ Global Health/Assets/Scripts/EventCardDeck.cs
@@ -2,38 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventCard : MonoBehaviour
-{
-    public string _name;
-    private string _description;
-    private int _choices;
-    GameObject _card;
-
-    public EventCard(string name, string description, int choices)
-    {
-        _name = name;
-        _description = description;
-        _choices = choices;
-    }
-}
-
 public class EventCardDeck : MonoBehaviour
 {
     public List<EventCard> deck = new List<EventCard>();
     public List<EventCard> usedPile = new List<EventCard>();
     public int eventTriggers = 0;
+    public EventCardDescription newDescription;
 
     void Start()
     {
         EventCard event0 = new EventCard("event0", "A car accident caused...", 3);
         deck.Add(event0);
 
-        // EventTrigger();
+        EventCard event1 = new EventCard("event1", "A tsunami hit the town...", 4);
+        deck.Add(event1);
+
+        EventCard event2 = new EventCard("event2", "People have become dissatisfied...", 2);
+        deck.Add(event2);
+
+        Shuffle();
+        TakeCard();
+    }
+
+    public void Shuffle()
+    {
+        for (int i = 0; i < deck.Count; i++)
+        {
+            var temp = deck[i];
+            Random.InitState(System.DateTime.Now.Millisecond);
+            int randomIndex = Random.Range(i, deck.Count);
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
     }
 
     public void EventTrigger()
     {
-        print("WORKED");
         int diceValue = Random.Range(1, 7);
         diceValue = diceValue + eventTriggers;
 
@@ -59,7 +63,7 @@ public class EventCardDeck : MonoBehaviour
         deck.RemoveAt(0);
         usedPile.Add(card);
 
-        print(card._name);
+        newDescription.LoadCard(card);
         return card;
     }
 }
--- Global Health/Assets/Scripts/Test.cs
+++ Global Health/Assets/Scripts/Test.cs
@@ -9,7 +9,7 @@ public class Test : MonoBehaviour
 
     void Start()
     {
-        test.EventTrigger();
+        // test.EventTrigger();
     }
 
     // Update is called once per frame
