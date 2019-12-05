cdb521aa78837cf94ddb695df595ff6452605b56
Mike Tetteroo <mike.tetteroo@hotmail.com>
2019-12-05 12:44:54 +0100

    Finished the second part of the tutorial to fix the "desync" problem

--- Global Health/Assets/Scripts/Networking/RandomizedPosition.cs
+++ Global Health/Assets/Scripts/Networking/RandomizedPosition.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class RandomizedPosition : MonoBehaviour
+public class RandomizedPosition : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Transform _child;
+
+    //This makes it so that the code is only ran on the client
+    //Think of it as a basic Start() method, but if one were to add this in there
+    //there would be a "desync" between the host and client
+    public override void OnStartLocalPlayer()
     {
-        transform.position += new Vector3(
+        base.OnStartLocalPlayer();
+        StartCoroutine(__RandomizePosition());
+    }
+
+    private IEnumerator __RandomizePosition()
+    {
+        WaitForSeconds wait = new WaitForSeconds(1f);
+        Vector3 startPosition = transform.position;
+
+        while (true)
+        {
+            transform.position = startPosition + ReturnRandom();
+            _child.localPosition = ReturnRandom();
+            yield return wait;
+        }
+    }
+
+    private Vector3 ReturnRandom()
+    {
+        return new Vector3(
         Random.Range(-4f, 4f),
         Random.Range(-4f, 4f),
         0f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
