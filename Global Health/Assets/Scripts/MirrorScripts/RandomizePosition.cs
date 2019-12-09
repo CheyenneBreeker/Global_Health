569a0911527db8e8e274ac1371765e7a8b71182e
DESKTOP-C0CBIQ5\angel <amoestadja@gmail.com>
2019-12-09 02:24:34 +0100

    deleted SocketWeaver. We should stick with Mirror. added mirror setup where players can join a room

---
+++ Global Health/Assets/Scripts/MirrorScripts/RandomizePosition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizePosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(_RandomizePosition());
+    }
+
+    private IEnumerator _RandomizePosition()
+    {
+        WaitForSeconds wait = new WaitForSeconds(1);
+        Vector3 startPosition = transform.position;
+
+        while (true)
+        {
+            transform.position = startPosition + ReturnRandom();
+            yield return wait;
+        }
+    }
+
+    private Vector3 ReturnRandom()
+    {
+        return new Vector3(
+            Random.Range(-4.0f, 4.0f),
+            Random.Range(-4.0f, 4.0f),
+            0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
