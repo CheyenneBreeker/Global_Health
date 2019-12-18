42dba4e8e1a4a4d70d8c9a8e7ea61d32184dce9c
Angelo Moestadja <amoestadja@gmail.com>
2019-12-18 10:12:13 +0100

    When the player inputs username and selects the host button, the username and its ip address gets stored in a php webserver

---
+++ Global Health/Assets/GetIP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetIP : MonoBehaviour
+{
+    string ipv4 = IPManager.GetIP(ADDRESSFAM.IPv4);
+
+    string ipv6 = IPManager.GetIP(ADDRESSFAM.IPv6);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(ipv4);
+        Debug.Log(ipv6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Global Health/Assets/Scripts/IPTransfer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class IPTransfer : MonoBehaviour
+{
+    public string username;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    string ipv4 = IPManager.GetIP(ADDRESSFAM.IPv4);
+
+    public void StoreName()
+    {
+        username = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome " + username + " " + ipv4;
+        StartCoroutine(GetUsername(username, ipv4));
+    }
+
+    IEnumerator GetUsername(string username, string ipaddress)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("userName", username);
+        form.AddField("userIP", ipaddress);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://epigastric-conseque.000webhostapp.com/GetIP.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            } else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}
