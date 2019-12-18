d2520f051d727f276c19dc759804fe5465178dce
Mike Tetteroo <mike.tetteroo@hotmail.com>
2019-12-18 11:40:53 +0100

    Finished the menu and cleaned up the code to be more adaptable if we ever need more "windows", also made it look nicer

--- Global Health/Assets/Scripts/MainMenuController.cs
+++ Global Health/Assets/Scripts/MainMenuController.cs
@@ -5,19 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject[] menus;
+
+    private int currentMenuNumber;
+    private int newMenuNumber;
+
+    void Start()
+    {
+        newMenuNumber = 0;
+    }
+
+    void Update()
+    {
+        //Check if we got a differen menu number
+        if (newMenuNumber == currentMenuNumber)
+        {
+            return;
+        }
+
+        //Make it the new current number and change the menu to it
+        currentMenuNumber = newMenuNumber;
+        ChangeMenu(currentMenuNumber);
+    }
+
+    //View the menu with the same menuNumber in de menus array
+    public void ChangeMenu(int menuNumber)
+    {
+        //Deactivate all opened menus
+        foreach (GameObject menu in menus)
+        {
+            menu.SetActive(false);
+        }
+
+        //Activate the correct menu
+        menus[menuNumber].SetActive(true);
+    }
+
+    //Set the new menu number (called by buttons)
+    public void SwitchMenuNumber(int menuNumber)
+    {
+        newMenuNumber = menuNumber;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("GameWorld");
-    }
-
-    public void ToggleOptionMenu()
-    {
-        Debug.Log("Toggle option menu.");
-    }
-
-    public void ToggleCredits()
-    {
-        Debug.Log("Toggle credits.");
     }
 
     public void QuitGame()
