e6c87c0b937f51c59d9fc6ae7e14ac5b39d7d654
Mike Tetteroo <mike.tetteroo@hotmail.com>
2019-12-19 13:52:03 +0100

    Made the options menu fully functional

--- Global Health/Assets/Scripts/SettingsScript.cs
+++ Global Health/Assets/Scripts/SettingsScript.cs
@@ -2,11 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
     public AudioMixer effectsAudioMixer;
     public AudioMixer ambientAudioMixer;
+
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        //Get all possible resolutions
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionIndex = 0;
+
+        //Unity dropdowns work with string, thus we make a new string-based list and convert the resolutions to string
+        List<string> resolutionOptions = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            resolutionOptions.Add(option);
+
+            //If the resolution is equal to our current resolution, remember it for later under currentResolutionIndex
+            if (resolutions[i].width == Screen.currentResolution.width &&
+            resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        //Add all the options to the dropdown
+        resolutionDropdown.AddOptions(resolutionOptions);
+
+        //Adjust the default resolution to be equal to our current resolution
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetSFXVolume(float volume)
     {
@@ -21,5 +56,16 @@ public class SettingsScript : MonoBehaviour
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
