d906bb2453f8ebeccab2f16c047b58c698c9fd69
Angelo Moestadja <amoestadja@gmail.com>
2020-01-08 12:05:56 +0100

    Edited ui endscreen

---
+++ Global Health/Assets/KeepMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeepMap : MonoBehaviour
+{
+
+    private Scene scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "MainMenu")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
