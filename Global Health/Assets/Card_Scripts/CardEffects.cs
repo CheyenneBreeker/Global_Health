f16cd5f95bfb5fd84acfbdb7e6f1c942ba188ea9
Mike Tetteroo <mike.tetteroo@hotmail.com>
2020-01-23 13:54:21 +0100

    Reverted a change to the CardEffects for improve health, rebalanced the "improve health" cards to be much cheaper and faster

--- Global Health/Assets/Card_Scripts/CardEffects.cs
+++ Global Health/Assets/Card_Scripts/CardEffects.cs
@@ -26,11 +26,7 @@ public class CardEffects : MonoBehaviour
         int affectedCity = (int)CardVariables.affectedCity;
         int cardValue = (int)CardVariables.cardValue;
         bool cardBool = (bool)CardVariables.NegativeValue;
-
-        string cardString = (string)CardVariables.AddedBuilding;
-        int CardTime = (int)CardVariables.TurnCountdown;
-
-        GameWorld.Instance.cities[affectedCity].ConstructBuilding(cardString,CardTime);
+        
         GameWorld.Instance.cities[affectedCity].UpdateHealthCare(cardValue, cardBool);
     }
 
